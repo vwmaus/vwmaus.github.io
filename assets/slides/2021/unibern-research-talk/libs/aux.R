0c5da4de7db1f56438b107a8a5a827f31445cebb
Victor Maus <vwmaus1@gmail.com>
2021-05-13 16:55:39 +0200

    Initial commit talk

---
+++ assets/slides/2021/unibern-research-talk/libs/aux.R
@@ -0,0 +1,6 @@
+
+system("drawio -x -t -o ./img/change-from-snapshots.svg ./img/change-from-snapshots.drawio")
+system("drawio -x -t -o ./img/change-from-timeseries.svg ./img/change-from-timeseries.drawio")
+system("drawio -x -t -o ./img/change-from-events.svg ./img/change-from-events.drawio")
+system("drawio -x -t -o ./img/from-ts-to-events.svg ./img/from-ts-to-events.drawio")
+system("drawio -x -t -o ./img/sits.svg ./img/sits.drawio")
