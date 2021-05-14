ad1670c84b61062b5cea16232f02065317c5bda6
Victor Maus <vwmaus1@gmail.com>
2021-05-14 18:52:13 +0200

    Adds mining slides

--- assets/slides/2021/unibern-research-talk/aux.R
+++ assets/slides/2021/unibern-research-talk/aux.R
@@ -3,4 +3,9 @@ system("drawio -x -t -o ./img/change-from-snapshots.svg ./img/change-from-snapsh
 system("drawio -x -t -o ./img/change-from-timeseries.svg ./img/change-from-timeseries.drawio")
 system("drawio -x -t -o ./img/change-from-events.svg ./img/change-from-events.drawio")
 system("drawio -x -t -o ./img/from-ts-to-events.svg ./img/from-ts-to-events.drawio")
+system("drawio -x -t -o ./img/from-ts-to-events-inverse.svg ./img/from-ts-to-events-inverse.drawio")
 system("drawio -x -t -o ./img/sits.svg ./img/sits.drawio")
+
+system("drawio -x -t -o ./img/land-cover-mapping-items.svg ./img/land-cover-mapping-items.drawio")
+system("drawio -x -t -o ./img/carajas-misclassification.svg ./img/carajas-misclassification.drawio")
+
