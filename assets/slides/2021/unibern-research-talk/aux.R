14cd9208ba31291283a583f4bf1ce239393b8719
Victor Maus <vwmaus1@gmail.com>
2021-05-17 21:10:01 +0200

    Script sci talk

--- assets/slides/2021/unibern-research-talk/aux.R
+++ assets/slides/2021/unibern-research-talk/aux.R
@@ -5,7 +5,15 @@ system("drawio -x -t -o ./img/change-from-events.svg ./img/change-from-events.dr
 system("drawio -x -t -o ./img/from-ts-to-events.svg ./img/from-ts-to-events.drawio")
 system("drawio -x -t -o ./img/from-ts-to-events-inverse.svg ./img/from-ts-to-events-inverse.drawio")
 system("drawio -x -t -o ./img/sits.svg ./img/sits.drawio")
-
 system("drawio -x -t -o ./img/land-cover-mapping-items.svg ./img/land-cover-mapping-items.drawio")
 system("drawio -x -t -o ./img/carajas-misclassification.svg ./img/carajas-misclassification.drawio")
 
+
+system("drawio -x -t -o ./img/change-from-snapshots.png ./img/change-from-snapshots.drawio")
+system("drawio -x -t -o ./img/change-from-timeseries.png ./img/change-from-timeseries.drawio")
+system("drawio -x -t -o ./img/change-from-events.png ./img/change-from-events.drawio")
+system("drawio -x -t -o ./img/from-ts-to-events.png ./img/from-ts-to-events.drawio")
+system("drawio -x -t -o ./img/from-ts-to-events-inverse.png ./img/from-ts-to-events-inverse.drawio")
+system("drawio -x -t -o ./img/sits.png ./img/sits.drawio")
+system("drawio -x -t -o ./img/land-cover-mapping-items.png ./img/land-cover-mapping-items.drawio")
+system("drawio -x -t -o ./img/carajas-misclassification.png ./img/carajas-misclassification.drawio")
