37d414276c0b57c2c1547d2bc9d59ba733cdf85e
vwmaus <vwmaus1@gmail.com>
2021-11-13 12:34:58 +0100

    Test light html

---
+++ assets/talks/2021-05-26-unibern-research-talk/aux.R
@@ -0,0 +1,25 @@
+
+# svg
+system("drawio -x -t -o ./img/change-from-snapshots.svg ./img/change-from-snapshots.drawio")
+system("drawio -x -t -o ./img/change-from-timeseries.svg ./img/change-from-timeseries.drawio")
+system("drawio -x -t -o ./img/change-from-events.svg ./img/change-from-events.drawio")
+system("drawio -x -t -o ./img/from-ts-to-events.svg ./img/from-ts-to-events.drawio")
+system("drawio -x -t -o ./img/from-ts-to-events-inverse.svg ./img/from-ts-to-events-inverse.drawio")
+system("drawio -x -t -o ./img/sits.svg ./img/sits.drawio")
+system("drawio -x -t -o ./img/land-cover-mapping-items.svg ./img/land-cover-mapping-items.drawio")
+system("drawio -x -t -o ./img/carajas-misclassification.svg ./img/carajas-misclassification.drawio")
+system("drawio -x -t -o ./img/twdtw.svg ./img/twdtw.drawio")
+
+# png
+system("drawio -x -t -o ./img/change-from-snapshots.png ./img/change-from-snapshots.drawio")
+system("drawio -x -t -o ./img/change-from-timeseries.png ./img/change-from-timeseries.drawio")
+system("drawio -x -t -o ./img/change-from-events.png ./img/change-from-events.drawio")
+system("drawio -x -t -o ./img/from-ts-to-events.png ./img/from-ts-to-events.drawio")
+system("drawio -x -t -o ./img/from-ts-to-events-inverse.png ./img/from-ts-to-events-inverse.drawio")
+system("drawio -x -t -o ./img/sits.png ./img/sits.drawio")
+system("drawio -x -t -o ./img/land-cover-mapping-items.png ./img/land-cover-mapping-items.drawio")
+system("drawio -x -t -o ./img/carajas-misclassification.png ./img/carajas-misclassification.drawio")
+system("drawio -x -t -o ./img/carajas-misclassification1.png ./img/carajas-misclassification1.drawio")
+system("drawio -x -t -o ./img/twdtw.png ./img/twdtw.drawio")
+system("drawio -x -t -o ./img/what-is-sds.png ./img/what-is-sds.drawio")
+system("drawio -x -t -o ./img/smartfarm.png ./img/smartfarm.drawio")
